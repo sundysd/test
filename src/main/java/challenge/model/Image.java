8e311043f4c91c91754f6ed03362717301b3be8a
Di Sun <sundysd@gmail.com>
2018-11-05 10:20:09 -0500

    Update Image.java

--- src/main/java/challenge/model/Image.java
+++ src/main/java/challenge/model/Image.java
@@ -1,4 +1,47 @@
 package challenge.model;
 
+
+import java.net.URI;
+
+
 public class Image {
+
+    String type;
+    URI url;
+    String height;
+    String width;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public URI getUrl() {
+        return url;
+    }
+
+    public void setUrl(URI url) {
+        this.url = url;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+
 }
