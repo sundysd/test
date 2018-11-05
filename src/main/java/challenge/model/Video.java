d01b5c542617c50e6d45fab054d34b5014d4fdd5
Di Sun <sundysd@gmail.com>
2018-11-05 10:12:52 -0500

    Update Video.java

--- src/main/java/challenge/model/Video.java
+++ src/main/java/challenge/model/Video.java
@@ -1,8 +1,39 @@
 package challenge.model;
 
+enum Source {
+    youtube,
+    vimeo;
+}
+
 public class Video {
 
+    String type;
+    String url;
+    Source source;
 
+    public String getType() {
+        return type;
+    }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+   
 
 }
