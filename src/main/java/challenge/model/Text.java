b1fec7308e243dc5df6810efdfb2a648e2f5d59b
Di Sun <sundysd@gmail.com>
2018-11-05 10:22:35 -0500

    Update Text.java

--- src/main/java/challenge/model/Text.java
+++ src/main/java/challenge/model/Text.java
@@ -1,4 +1,25 @@
 package challenge.model;
 
 public class Text {
+
+    String type;
+    String text;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
 }
